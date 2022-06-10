c6d4128d19d5a206aa6996b2174458f5b1071117
Yogender Sharma <yogender.sharma@citixsys.com>
2022-06-11 01:08:54 +0530

    DbContext class added, connection string added

---
+++ BookStore.API/Data/BookStoreContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            :base(options)
+        { 
+
+        }
+
+        public DbSet<Books> Books { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=CXS-YOGENDER;Database=BookStoreAPI;Trusted_Connection=True;MultipleActiveResultSets=true;User ID=sa;Password=pass;Persist Security Info=True");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
+ 
--- BookStore.API/Startup.cs
+++ BookStore.API/Startup.cs
@@ -1,8 +1,10 @@
+using BookStore.API.Data;
 using BookStore.API.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,12 +29,16 @@ namespace BookStore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<BookStoreContext>(
+            //    options => options.UseSqlServer("Server=CXS-YOGENDER;Database=BookStoreAPI;Trusted_Connection=True;User ID=sa;Password=pass;Persist Security Info=True"));
 
+            services.AddDbContext<BookStoreContext>(
+               options => options.UseSqlServer(Configuration.GetConnectionString("BookStoreDB")));
             services.AddControllers();
             services.AddTransient<IBookRepository, BookRepository>();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore.API", Version = "v1" });
+                //c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore.API", Version = "v1" });
             });
 
         }
